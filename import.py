0ccd755167ac042c5ac3d6efca5ffc8dc4af92cb
Borith MOEK <53550645+borith23@users.noreply.github.com>
2020-08-26 14:05:51 +0700

    module import

--- import.py
+++ import.py
@@ -1,3 +1,6 @@
 import myModule
 
 myModule.greeting("Rith")
+
+a = myModule.person1["age"]
+print(a)
--- myModule.py
+++ myModule.py
@@ -2,3 +2,9 @@
 def greeting(name):
 
     print("Hello, " + name)
+
+person1 = {
+    "name": "Rith",
+    "age": 21,
+    "country": "Cambodia"
+}
