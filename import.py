0200305b904a4525ffce8184b1cf2fa0be36f19b
Borith MOEK <53550645+borith23@users.noreply.github.com>
2020-08-26 14:29:11 +0700

    get real time from python

--- import.py
+++ import.py
@@ -1,6 +1,10 @@
 import myModule
+import datetime
 
-myModule.greeting("Rith")
+# myModule.greeting("Rith")
 
-a = myModule.person1["age"]
-print(a)
+# a = myModule.person1["age"]
+# print(a)
+
+getTime = datetime.datetime.now()
+print(getTime.year, getTime.strftime("%a"), getTime.month)
