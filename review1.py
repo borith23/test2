6f6175ee269804575a2ad95b3e84371d4173838d
Borith MOEK <53550645+borith23@users.noreply.github.com>
2020-08-31 10:38:08 +0700

    review loop

--- review1.py
+++ review1.py
@@ -27,6 +27,27 @@ class Teacher(Person):
         self.gender = sex 
 
     def showTeacher(self):
-        print("I'm name is", self.firstName, self.lastName, "and Gender:", self.gender)
+        print("My name is", self.firstName, self.lastName, "and Gender:", self.gender)
 nameTeacher = Teacher("Rady", "Y", "male")
 nameTeacher.showTeacher()
+
+my_list = [1, 2, 3, 4, 5]
+my_iter = iter(my_list)
+
+print(next(my_iter))
+print(next(my_iter))
+print(my_iter.__next__())   
+print(my_list)
+print(next(my_iter))    
+print(my_iter.__next__())  
+
+print("=========>*<==========")
+
+for x in my_list:
+    print(x)
+
+print("=========>*<==========")
+
+length = len(my_list)
+for i in range(length): 
+    print(my_list[i]) 
